d78539d3ad694391e1b7bea732a82edfb4baa4f9
Jonathan Cameron <jacc@bu.edu>
2020-09-16 16:36:43 +0100

    Plots

--- Report.md
+++ Report.md
@@ -4,3 +4,18 @@ The welcome message sent to the client upon initial conncetion with the server i
 
 # Task 1
 
+I added code to client.py so that the data the client receives is placed in a file, the file name being an argument passed to the program in the command line. I wrote the JSON data to the file be using file handles, rather than changing standard out from the command line to the file. This meant that the JSON data was printed to the command line and also written to the file.
+
+# Task 2
+
+Using statistics and scipy.stats I calculated the median and variance of the temperature and occupancy data for class1 and plotted the Probability Density Function (PDF) of the temperature, occupancy and co2 levels of the same room. 
+
+# Occupancy PDF
+![image info](./media/occuPDF.png)
+
+# Temperature PDF
+![image info](./media/tempPDF.png)
+
+# CO2 PDF
+![image info](./media/co2PDF.png)
+
--- analyse.py
+++ analyse.py
@@ -16,6 +16,8 @@ from datetime import datetime
 import typing as T
 import matplotlib.pyplot as plt
 import numpy as np
+import statistics as statistics
+from scipy.stats import norm
 
 
 def load_data(file: Path) -> T.Dict[str, pandas.DataFrame]:
@@ -32,7 +34,9 @@ def load_data(file: Path) -> T.Dict[str, pandas.DataFrame]:
 
             temperature[time] = {room: r[room]["temperature"][0]}
             occupancy[time] = {room: r[room]["occupancy"][0]}
-            co2[time] = {room: r[room]["co2"][0]}
+            co2[time] = {room: r[room]["co2"][0]}\
+    
+    
 
     data = {
         "temperature": pandas.DataFrame.from_dict(temperature, "index").sort_index(),
@@ -52,12 +56,47 @@ if __name__ == "__main__":
 
     data = load_data(file)
 
-    for k in data:
-        # data[k].plot()
-        time = data[k].index
-        data[k].hist()
-        plt.figure()
-        plt.hist(np.diff(time.values).astype(np.int64) // 1000000000)
-        plt.xlabel("Time (seconds)")
+    tempData = data['temperature']['class1']
+    occuData = data['occupancy']['class1']
+    co2Data = data['co2']['class1']
 
+    tempData = tempData.dropna()
+    occuData = occuData.dropna()
+    co2Data = co2Data.dropna()
+
+    tempData = tempData.sort_values(axis='index', ascending=False)
+    occuData = occuData.sort_values(axis='index', ascending=False)
+    co2Data = co2Data.sort_values(axis='index', ascending=False)
+
+    medianTempData = statistics.median(tempData)
+    medianOccuData = statistics.median(occuData)
+
+    varianceTempData = statistics.variance(tempData)
+    varianceOccuData = statistics.variance(occuData)
+
+    
+    print('The median temperature of class1 is: ')
+    print(medianTempData)
+
+    print('The variance of the temperature data of class1 is: ')
+    print(varianceTempData)
+
+    print('The median occupancy of class1 is: ')
+    print(medianOccuData)
+
+    print('The variance of the occupancy data of class1 is: ')
+    print(varianceOccuData)
+
+    fig1 = plt.subplot(1, 1, 1) 
+    fig1.plot(occuData, norm.pdf(occuData), label='Occupancy PDF')
     plt.show()
+
+    fig2 = plt.subplot(1, 1, 1)
+    fig2.plot(tempData, norm.pdf(tempData), label='Temperature PDF')
+    plt.show()
+
+    fig3 = plt.subplot(1, 1, 1)
+    fig3.plot(co2Data, norm.pdf(co2Data), label='CO2 PDF')
+    plt.show()
+
+    
